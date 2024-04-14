62fcb3566b9c15171e0da4f66fe9404656904c59
modusfas <modupefasunon@gmail.com>
2024-04-14 22:08:14 +0000

    i18n

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
